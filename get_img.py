e0e53f2c89c88d4e5c1453115a95f3ae351ac931
SZ7M8ci7 <83046414+SZ7M8ci7@users.noreply.github.com>
2025-02-10 01:18:59 +0900

    test

--- get_img.py
+++ get_img.py
@@ -9,9 +9,12 @@ def get_img(filename):
         time.sleep(1)
         url = "https://twst.wikiru.jp/attach2/696D67_" + filename.encode('utf-8').hex().rstrip().upper()+".jpg"
         r = requests.get(url)
+        if 200 != r.status_code:
+            return
         path = 'get/' + filename
         image_file = open(path, 'wb')
         image_file.write(r.content)
+        print(r.content)
         image_file.close()
     except:
         pass
@@ -31,3 +34,4 @@ if __name__ == '__main__':
         filename = f"{d['rare']}{d['chara']}【{d['costume']}】アイコン.jpg"
         if filename not in exists_files:
             get_img(filename)
+            break
